b6d5e95402ee6c519dedfb53f264a29e5c5ee6a0
mathemator <mathemator@yandex.ru>
2025-05-08 07:46:25 +0300

    init rabbit template

---
+++ src/main/java/ru/mathemator/rabbitmq/demo/rabbit/RabbitConfig.java
@@ -0,0 +1,27 @@
+package ru.mathemator.rabbitmq.demo.rabbit;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public Queue queue() {
+        return new Queue("myQueue", false);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue()).to(exchange()).with("myRoutingKey");
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange("myExchange");
+    }
+}
